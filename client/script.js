179f3fd2bae65aa9cf858befdd7015783b14d36e
Orest Bahlay <orest@MacBook-Pro-Orest.local>
2024-12-18 13:20:29 +0200

    Connecting choice of model to the frontend

--- client/script.js
+++ client/script.js
@@ -1,4 +1,5 @@
 function performPrediction() {
+    const model_name = document.getElementById('model_name').value.trim();
     const g1 = document.getElementById('g1').value.trim();
     const g2 = document.getElementById('g2').value.trim();
     const medu = document.getElementById('medu').value.trim();
@@ -8,22 +9,23 @@ function performPrediction() {
     const internet_yes = document.getElementById('internet_yes').value.trim();
     const higher_yes = document.getElementById('higher_yes').value.trim();
 
-    if (!g1 || !g2 || !medu || !fedu || !failures || !studytime || !internet_yes || !higher_yes) {
+    if (!g1 || !g2 || !medu || !fedu || !failures || !studytime || !internet_yes || !higher_yes || !model_name) {
         document.getElementById('result').innerText = 'Please fill in all fields.';
         return;
     }
 
-    const input = [g1, g2, medu, fedu, failures, studytime, internet_yes, higher_yes];
+    const input = [model_name, g1, g2, medu, fedu, failures, studytime, internet_yes, higher_yes];
 
     const formData = new FormData();
-    formData.append('g1', input[0]);
-    formData.append('g2', input[1]);
-    formData.append('medu', input[2]);
-    formData.append('fedu', input[3]);
-    formData.append('failures', input[4]);
-    formData.append('studytime', input[5]);
-    formData.append('internet_yes', input[6]);
-    formData.append('higher_yes', input[7]);
+    formData.append('model_name', model_name);
+    formData.append('g1', input[1]);
+    formData.append('g2', input[2]);
+    formData.append('medu', input[3]);
+    formData.append('fedu', input[4]);
+    formData.append('failures', input[5]);
+    formData.append('studytime', input[6]);
+    formData.append('internet_yes', input[7]);
+    formData.append('higher_yes', input[8]);
 
     fetch('http://127.0.0.1:5000/predict_grade', { // URL до Flask
         method: 'POST',
